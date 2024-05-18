13f4e46ce22b27d08164ce82b0d1cda4f281265d
Cloverix <168465860+Cloverix@users.noreply.github.com>
2024-05-18 12:40:25 +0300

    Add files via upload

---
+++ Vec increace.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <vector>
+
+void increase(std::vector<int>& vec, int n) {
+	for (int i = 0; i < vec.size(); i++) {
+		vec[i] += n;
+	}
+}
+
+int main() {
+	std::vector<int> v = { 1, 2, 3, 4, 5 };
+	increase(v, 5);
+	for (int i = 0; i < v.size(); i++) {
+		std::cout << v[i] << " ";
+	}
+}
